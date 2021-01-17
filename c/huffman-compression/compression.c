a711b5163dfb9a5c3dce05a2b6ff21e25bb8844d
joshuajprice <joshua.j.price@my.wheaton.edu>
2021-01-16 21:01:42 -0800

    continued work on compression

--- c/huffman-compression/compression.c
+++ c/huffman-compression/compression.c
@@ -2,6 +2,10 @@
 #include <getopt.h>
 #include <string.h>
 
+
+#define SIZE_OF_MAP 32
+
+
 int main(int argc, char *argv[ ])
 {
     if (argc != 2)   {
@@ -9,11 +13,25 @@ int main(int argc, char *argv[ ])
         return 1;
     }
 
+
+    char ascii[256];
     int i;
 
     char filename[256];
     strcpy(filename, argv[1]);
 
+    FILE *file = fopen(filename, 'r');
     
+
+    char c;
+
+    if(file){
+        while(fscanf(file, '%c', c)){
+            ascii[(int)c]++;
+        }
+    }
+
+    
+
     return 0;
 }
