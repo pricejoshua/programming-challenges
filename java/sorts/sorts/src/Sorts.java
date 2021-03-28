ece17b331d9b88f00688bf1d264e9dcd2f355480
joshuajprice <joshua.j.price@my.wheaton.edu>
2021-03-28 17:38:27 -0500

    Started the sorting algorithm

---
+++ java/sorts/sorts/src/Sorts.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+import java.util.Random;
+
+public class Sorts {
+    public static void main(String[] args){
+        
+    }
+
+
+    public int[] populateArray(int len, int range){
+        int[] toReturn = new int[len];
+        Random rand = new Random();
+        for (int i = 0; i < len; i++){
+            toReturn[i] = rand.nextInt(range);
+        }
+        return toReturn;
+    }
+}
